b5ca6aa029f6d559a23f6dd463a921cdc1f3b52e
cloune <rachel@nebulae.co>
2018-10-09 17:15:59 +0200

    create user and user controller

---
+++ src/controllers/UserController.py
@@ -0,0 +1,23 @@
+from src.models.User import *
+
+class UserController:
+
+    @classmethod
+    def get_one(cls, id_user : int):
+        #TODO
+        pass
+
+    @classmethod
+    def add_user(cls,firstname: str, surname: str, login: str, pwd: str, poster: str,
+                 list_preferences: List[Serie]=None):
+        User.create_user_in_bdd(firstname,surname,login,pwd,poster,list_preferences)
+
+    @classmethod
+    def update_user(cls,user : User, firstname: str=None, surname: str=None, login: str=None, pwd: str=None, poster: str=None,
+                 list_preferences: List[Serie]=None):
+        user.update_user_in_bdd(firstname, surname, login, pwd, poster, list_preferences)
+
+    @classmethod
+    def del_user(cls, user: User):
+        user.delete_user_in_bdd()
+
---
+++ src/models/User.py
@@ -0,0 +1,106 @@
+from src.models.Serie import *
+from typing import List
+
+
+class User:
+
+    def __init__(self, id: int, firstname: str, surname: str, login: str, pwd: str, poster: str,
+                 list_preferences: List[Serie]=None):
+        self.__id = id
+        self.__firstname = firstname
+        self.__surname = surname
+        self.__login = login
+        self.__pwd = pwd
+        self.__poster = poster
+        self.__list_preferences = list_preferences
+
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def firstname(self):
+        return self.__firstname
+
+    @firstname.setter
+    def firstname(self, value):
+        if type(value) is not str:
+            raise TypeError("Firstname should be a string")
+        else:
+            self.__firstname = value
+
+    @property
+    def surname(self):
+        return self.__surname
+
+    @surname.setter
+    def surname(self, value):
+        if type(value) is not str:
+            raise TypeError("Surname should be a string")
+        else:
+            self.__surname = value
+
+    @property
+    def login(self):
+        return self.__login
+
+    @login.setter
+    def login(self, value):
+        if type(value) is not str:
+            raise TypeError("Login should be a string")
+        else:
+            self.__login = value
+
+    @property
+    def pwd(self):
+        return self.__pwd
+
+    @pwd.setter
+    def pwd(self, value):
+        if type(value) is not str:
+            raise TypeError("Password should be a string")
+        else:
+            self.__pwd = value
+
+    @property
+    def poster(self):
+        return self.__poster
+
+    @poster.setter
+    def poster(self, value):
+        if type(value) is not str:
+            raise TypeError("Poster url should be a string")
+        else:
+            self.__poster = value
+
+    @property
+    def list_preferences(self):
+        return self.__list_preferences
+
+    @list_preferences.setter
+    def list_preferences(self, value):
+        if type(value) is not List[Serie]:
+            raise TypeError("Poster url should be a string")
+        else:
+            self.__list_preferences = value
+
+    @classmethod
+    def create_user_in_bdd(cls, firstname: str, surname: str, login: str, pwd: str, poster: str,
+                           list_preferences: List[Serie]):
+        # TODO requete SQL
+        # id = USERINBDD (id)
+        return cls(id, firstname, surname, login, pwd, poster, list_preferences)
+
+    def update_user_in_bdd(self, firstname: str = None, surname: str = None, login: str = None, pwd: str = None,
+                           poster: str = None, list_preferences: List[Serie] = None):
+        # TODO requete SQL
+        self.__firstname = firstname
+        self.__surname = surname
+        self.__login = login
+        self.__pwd = pwd
+        self.__poster = poster
+        self.__list_preferences = list_preferences
+
+    def delete_user_in_bdd(self):
+        #TODO requete SQL
+        del self
