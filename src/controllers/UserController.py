495f40cd219148bca7b1979fc625b402c9731beb
cloune <rachel@nebulae.co>
2018-10-12 10:32:57 +0200

    correct from review

--- src/controllers/UserController.py
+++ src/controllers/UserController.py
@@ -1,6 +1,11 @@
 from src.models.User import *
+from src.models.Show import *
 
 class UserController:
+
+    """
+        This class is our UserController
+    """
 
     @classmethod
     def get_one(cls, id_user : int):
@@ -9,13 +14,13 @@ class UserController:
 
     @classmethod
     def add_user(cls,firstname: str, surname: str, login: str, pwd: str, poster: str,
-                 list_preferences: List[Serie]=None):
+                 list_preferences: List[Show]=None):
         user= User(firstname,surname, login, pwd, poster, list_preferences)
         user.create_user_in_bdd()
 
     @classmethod
     def update_user(cls,user : User, firstname: str=None, surname: str=None, login: str=None, pwd: str=None, poster: str=None,
-                 list_preferences: List[Serie]=None):
+                 list_preferences: List[Show]=None):
         user.update_user_in_bdd(firstname, surname, login, pwd, poster, list_preferences)
 
     @classmethod
--- src/models/User.py
+++ src/models/User.py
@@ -1,31 +1,33 @@
-from src.models.Serie import *
+from src.models.Show import *
 from typing import List
 
 
 class User:
 
+    """
+        This class represents our User and is populated by the database
+    """
+
     def __init__(self, firstname: str, surname: str, login: str, pwd: str, poster: str,
-                 list_preferences: List[Serie]=None, id:int = None):
-        self.__id = id
-        self.__firstname = firstname
-        self.__surname = surname
-        self.__login = login
-        self.__pwd = pwd
-        self.__poster = poster
-        self.__list_preferences = list_preferences
+                 list_preferences: List[Show] =None, id:int = None):
+        self.__setId(id)
+        self.firstname = firstname
+        self.surname = surname
+        self.__setLogin(login)
+        self.__setPwd(pwd)
+        self.poster = poster
+        self.list_preferences = list_preferences
+
 
     @property
     def id(self):
         return self.__id
 
-    @id.setter
-    def id(self,value):
-        if value == None :
-            self.__id=0
-        elif type(value) is not int:
+    def __setId(self,id):
+        if type(id) is not int and id is not None:
             raise TypeError("Id should be an integer")
         else :
-            self.__id = value
+            self.__id = id
 
 
 
@@ -34,66 +36,64 @@ class User:
         return self.__firstname
 
     @firstname.setter
-    def firstname(self, value):
-        if type(value) is not str:
+    def firstname(self, firstname):
+        if type(firstname) is not str:
             raise TypeError("Firstname should be a string")
         else:
-            self.__firstname = value
+            self.__firstname = firstname
 
     @property
     def surname(self):
         return self.__surname
 
     @surname.setter
-    def surname(self, value):
-        if type(value) is not str:
+    def surname(self, surname):
+        if type(surname) is not str:
             raise TypeError("Surname should be a string")
         else:
-            self.__surname = value
+            self.__surname = surname
 
     @property
     def login(self):
         return self.__login
 
-    @login.setter
-    def login(self, value):
-        if type(value) is not str:
+    def __setLogin(self, login):
+        if type(login) is not str:
             raise TypeError("Login should be a string")
         else:
-            self.__login = value
+            self.__login = login
 
     @property
     def pwd(self):
         return self.__pwd
 
-    @pwd.setter
-    def pwd(self, value):
-        if type(value) is not str:
+    def __setPwd(self, pwd):
+        if type(pwd) is not str:
             raise TypeError("Password should be a string")
         else:
-            self.__pwd = value
+            self.__pwd = pwd
 
     @property
     def poster(self):
         return self.__poster
 
     @poster.setter
-    def poster(self, value):
-        if type(value) is not str:
+    def poster(self, poster):
+        if type(poster) is not str:
             raise TypeError("Poster url should be a string")
         else:
-            self.__poster = value
+            self.__poster = poster
 
     @property
     def list_preferences(self):
         return self.__list_preferences
 
     @list_preferences.setter
-    def list_preferences(self, value):
-        if type(value) is not List[Serie]:
-            raise TypeError("Poster url should be a string")
+    def list_preferences(self, list_preferences):
+        if type(list_preferences) is not List[Show] and list_preferences is not None:
+            raise TypeError("List of preferences should be a list of preferences")
         else:
-            self.__list_preferences = value
+            self.__list_preferences = list_preferences
 
 
     def create_user_in_bdd(self):
@@ -102,15 +102,26 @@ class User:
         # id = USERINBDD (id)
 
     def update_user_in_bdd(self, firstname: str = None, surname: str = None, login: str = None, pwd: str = None,
-                           poster: str = None, list_preferences: List[Serie] = None):
+                           poster: str = None, list_preferences: List[Show]= None):
         # TODO requete SQL
-        self.__firstname = firstname
-        self.__surname = surname
-        self.__login = login
-        self.__pwd = pwd
-        self.__poster = poster
-        self.__list_preferences = list_preferences
+        if firstname is not None :
+            self.firstname = firstname
+        if surname is not None:
+            self.surname = surname
+        if login is not None:
+            self.__setLogin(login)
+        if pwd is not None :
+            self.__setPwd(pwd)
+        if poster is not None :
+            self.poster = poster
+        if list_preferences is not None :
+            self.list_preferences = list_preferences
 
     def delete_user_in_bdd(self):
         #TODO requete SQL
         del self
+
+    @classmethod
+    def retrieve_user_from_credentials(cls, login, pwd):
+        pass
+        #TODO SQL REQUEST
