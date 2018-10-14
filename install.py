89f24458dc688e6dc76e1e240fac8f930026dd12
Kouskastook <fley58@gmail.com>
2018-10-14 23:59:16 +0200

    23(conf): fix install.sh

--- install.py
+++ install.py
@@ -1,7 +1,12 @@
 from src.db.MyDBConnection import MyDBConnection
+import os
+
+DB_FILENAME = 'db/gotCrawler.db'
+
+if os.path.isfile(DB_FILENAME) and input("Woops, a database already exist, remove it ? (y/n) : ") == 'y':
+    os.remove(DB_FILENAME)
 
 with open('db/create_and_seed.sql') as sql_file:
-    my_db_connection = MyDBConnection('db/gotCrawler.db')
+    my_db_connection = MyDBConnection(DB_FILENAME)
     queries = sql_file.read().split(';')
     my_db_connection.exec_mul(queries)
-    print([i for i in my_db_connection.exec_one('SELECT * from user')])
--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
 echo "Installing virtual environment and requirements."
-pip install --upgrade pip
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    sudo apt-get install python3-venv
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "Python3-venv should be on your machine, if there is an issue, please install it."
+else
+    echo "Please use MACOS or Linux, you may refer to README.md"
+fi
 python3 -m venv venv
-source venv/bin/activate
+. venv/bin/activate
+pip install --upgrade pip
 pip install -r requirements.txt
-echo "Now creating the example database."
+
+echo "Now installing the example database."
 python3 install.py
 echo "You may now use the script: launch.sh to launch the app."
