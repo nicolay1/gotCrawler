5da4bb2d220fba2c946989862fbba95cf67a8e1f
cloune <rachel@nebulae.co>
2018-10-18 15:33:56 +0200

    make season setters private

--- src/models/Season.py
+++ src/models/Season.py
@@ -9,16 +9,15 @@ class Season:
     """
 
     def __init__(self, id_show: int, num_season: int, list_episodes: List[Episode]):
-        self.id_show = id_show
-        self.num_season = num_season
-        self.list_episodes = list_episodes
+        self.__set_id_show( id_show)
+        self.__set_num_season(num_season)
+        self.__set_list_episodes(list_episodes)
 
     @property
     def id_show(self):
         return self.__id_show
 
-    @id_show.setter
-    def id_show(self, id_show : int):
+    def __set_id_show(self, id_show : int):
         if type(id_show) is not int:
             raise TypeError("Show id must be an integer")
         else:
@@ -28,8 +27,7 @@ class Season:
     def num_season(self):
         return self.__num_season
 
-    @num_season.setter
-    def num_season(self, num_season : int):
+    def __set_num_season(self, num_season : int):
         if type(num_season) is not int:
             raise TypeError("Season number must be an integer")
         else:
@@ -39,8 +37,7 @@ class Season:
     def list_episodes(self):
         return self.__list_episodes
 
-    @list_episodes.setter
-    def list_episodes(self, list_episodes : List[Episode]):
+    def __set_list_episodes(self, list_episodes : List[Episode]):
         if type(list_episodes) is not List[Episode]:
             raise TypeError("Episodes list must be a list of episodes")
         else:
