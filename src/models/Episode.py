82612d761522658316e5c7e8ee866bdd120bb366
cloune <rachel@nebulae.co>
2018-10-18 15:39:22 +0200

    make episode setters private and correct TODO in episode

--- src/models/Episode.py
+++ src/models/Episode.py
@@ -1,8 +1,9 @@
 from typing import List
+from src.models.Actor import *
+from src.models.Author import *
 
 
 class Episode:
-
     """
         This class represents an episode from a show, populated from the api
     """
@@ -10,19 +11,18 @@ class Episode:
     def __init__(self, name: str, id_season: int, num_ep: int, summary: str, list_actor: List[Actor],
                  list_author: List[Author]):
 
-        self.name = name
-        self.id_season = id_season
-        self.num_ep = num_ep
-        self.summary = summary
-        self.list_actor = list_actor
-        self.list_author = list_author
+        self.__set_name(name)
+        self.__set_id_season(id_season)
+        self.__set_num_ep(num_ep)
+        self.__set_summary(summary)
+        self.__set_list_actor(list_actor)
+        self.__set_list_author(list_author)
 
     @property
     def name(self):
         return self.__name
 
-    @name.setter
-    def name(self, name : str):
+    def __set_name(self, name: str):
         if type(name) is not str:
             raise TypeError("Name should be a string")
         else:
@@ -32,8 +32,7 @@ class Episode:
     def id_season(self):
         return self.__id_season
 
-    @id_season.setter
-    def id_season(self, id_season : int):
+    def __set_id_season(self, id_season: int):
         if type(id_season) is not int:
             raise TypeError("Season Id must be an integer")
         else:
@@ -43,8 +42,7 @@ class Episode:
     def num_ep(self):
         return self.__num_ep
 
-    @num_ep.setter
-    def num_ep(self, num_ep : int):
+    def __set_num_ep(self, num_ep: int):
         if type(num_ep) is not int:
             raise TypeError("Episode number must be an integer")
         else:
@@ -54,8 +52,7 @@ class Episode:
     def summary(self):
         return self.__summary
 
-    @summary.setter
-    def summary(self, summary):
+    def __set_summary(self, summary):
         if type(summary) is not str:
             raise TypeError("Summary should be a string")
         else:
@@ -65,9 +62,8 @@ class Episode:
     def list_actor(self):
         return self.__list_actor
 
-    @list_actor.setter
-    def list_actor(self, list_actor : List):
-        if type(list_actor) is not List:  # TODO
+    def __set_list_actor(self, list_actor: List[Actor]):
+        if type(list_actor) is not List[Actor]:
             raise TypeError("Actor's list should be a list")
         else:
             self.__list_actor = list_actor
@@ -76,9 +72,8 @@ class Episode:
     def list_author(self):
         return self.__list_author
 
-    @list_author.setter
-    def list_author(self, list_author : List):
-        if type(list_author) is not List:  # TODO
+    def __set_list_author(self, list_author: List[Author]):
+        if type(list_author) is not List[Author]:
             raise TypeError("Author's list should be a list")
         else:
             self.__list_author = list_author
