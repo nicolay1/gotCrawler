a3be8bec78d0c60233c0c9350730c4b17a642678
cloune <rachel@nebulae.co>
2018-10-23 16:11:36 +0200

    call to api in season controller + estethic change

--- src/api_helper/ApiHelperTMDB.py
+++ src/api_helper/ApiHelperTMDB.py
@@ -5,10 +5,12 @@ from src.models.Season import Season
 from src.config import CONFIG
 from src.api_helper.ApiHelper import ApiHelper
 
+
 class ApiHelperTMDB(ApiHelper):
     """
         TMDB Api Caller
     """
+
     def __init__(self):
         super().__init__(
             CONFIG["tmdb_api_root"],
@@ -18,14 +20,14 @@ class ApiHelperTMDB(ApiHelper):
         )
 
     def get_trending(self, page=1):
-        return self._get("trending/tv/week", None, {"page":page})
+        return self._get("trending/tv/week", None, {"page": page})
 
     def get_show(self, show_id: int):
         return self._get("tv/{}", (show_id))
 
     def get_season(self, show_id: int, season_number: int):
-        json= self._get("tv/{}/season/{}", (show_id, season_number))
-        self._api_json_to_season(json,show_id)
+        json = self._get("tv/{}/season/{}", (show_id, season_number))
+        self._api_json_to_season(json, show_id)
 
     def get_episode(self, show_id: int, season_number: int, episode_number: int):
         return self._get("tv/{}/season/{}/episode/{}", (show_id, season_number, episode_number))
@@ -33,15 +35,16 @@ class ApiHelperTMDB(ApiHelper):
     def _api_json_to_show(self, show_json: Dict):
         raise NotImplementedError
 
-    def _api_json_to_season(self, season_json: Dict, id_show:int):
-        name= season_json["name"]
-        overview=season_json["overview"]
-        poster=season_json["poster_path"]
-        num_season=season_json["season_number"]
-        list_episodes=[]
+    def _api_json_to_season(self, season_json: Dict, id_show: int):
+        name = season_json["name"]
+        overview = season_json["overview"]
+        poster = season_json["poster_path"]
+        num_season = season_json["season_number"]
+        list_episodes = []
         for json in season_json["episodes"]:
             list_episodes.append(self._api_json_to_episode(json))
-        return Season(id_show=id_show,num_season=num_season,list_episodes=list_episodes,name=name,poster=poster,overview=overview)
+        return Season(id_show=id_show, num_season=num_season, list_episodes=list_episodes, name=name, poster=poster,
+                      overview=overview)
 
     def _api_json_to_episode(self, episode_json: Dict):
         raise NotImplementedError
--- src/models/Season.py
+++ src/models/Season.py
@@ -3,13 +3,13 @@ from src.models.Episode import *
 
 
 class Season:
-
     """
         This class represents a season of a show, populated from api
     """
 
-    def __init__(self, id_show: int, num_season: int, list_episodes: List[Episode], name : str, poster : str, overview : str):
-        self.__set_id_show( id_show)
+    def __init__(self, id_show: int, num_season: int, list_episodes: List[Episode], name: str, poster: str,
+                 overview: str):
+        self.__set_id_show(id_show)
         self.__set_num_season(num_season)
         self.__set_list_episodes(list_episodes)
         self.__set_name(name)
@@ -20,7 +20,7 @@ class Season:
     def id_show(self):
         return self.__id_show
 
-    def __set_id_show(self, id_show : int):
+    def __set_id_show(self, id_show: int):
         if type(id_show) is not int:
             raise TypeError("Show id must be an integer")
         else:
@@ -30,7 +30,7 @@ class Season:
     def num_season(self):
         return self.__num_season
 
-    def __set_num_season(self, num_season : int):
+    def __set_num_season(self, num_season: int):
         if type(num_season) is not int:
             raise TypeError("Season number must be an integer")
         else:
@@ -40,7 +40,7 @@ class Season:
     def list_episodes(self):
         return self.__list_episodes
 
-    def __set_list_episodes(self, list_episodes : List[Episode]):
+    def __set_list_episodes(self, list_episodes: List[Episode]):
         if type(list_episodes) is not List[Episode]:
             raise TypeError("Episodes list must be a list of episodes")
         else:
@@ -50,7 +50,7 @@ class Season:
     def name(self):
         return self.__name
 
-    def __set_name(self,name : str):
+    def __set_name(self, name: str):
         if type(name) is not str:
             raise TypeError("Season name must be string")
         else:
@@ -60,7 +60,7 @@ class Season:
     def poster(self):
         return self.__poster
 
-    def __set_poster(self,poster : str):
+    def __set_poster(self, poster: str):
         if type(poster) is not str:
             raise TypeError("Season poster must be string")
         else:
@@ -70,7 +70,7 @@ class Season:
     def overview(self):
         return self.__overview
 
-    def __set_overview(self,overview : str):
+    def __set_overview(self, overview: str):
         if type(overview) is not str:
             raise TypeError("Season overview must be string")
         else:
