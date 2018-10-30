55fe1db372a440174dd702fe29bfb51d1d11eac3
Kouskastook <fley58@gmail.com>
2018-10-30 15:46:34 +0100

    chore(bootstrap_vue): added bootstrap vue

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,28 +1,13 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <ShowCard/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ShowCard from './components/ShowCard.vue'
 export default {
-  name: 'app',
+  name:'App',
   components: {
-    HelloWorld
+    ShowCard
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- front/src/main.js
+++ front/src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
+import BootstrapVue from "bootstrap-vue"
 import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
 
-Vue.config.productionTip = false
+Vue.use(BootstrapVue)
 
 new Vue({
+  el: '#app',
+  components: {App},
   render: h => h(App)
-}).$mount('#app')
+})
