e13fd364366c2ee97df887ef7ac77e6b59a74079
Chapuis Amelie <amelie.chapuis@student.ecp.fr>
2018-11-06 10:04:53 +0100

    creation of author and actor components

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,13 +1,93 @@
 <template>
-  <ShowCard/>
-</template>
 
+    <div class="container">
+        <ActorList
+                :actors= actor_list>
+        </ActorList>
+        <AuthorList
+                :authors= author_list>
+        </AuthorList>
+    </div>
+</template>
 <script>
-import ShowCard from './components/ShowCard.vue'
-export default {
-  name:'App',
-  components: {
-    ShowCard
-  }
-}
+    import ActorList from "./components/ActorList.vue";
+    import AuthorList from "./components/AuthorList.vue";
+    export default {
+        name: 'App',
+        components: {
+            ActorList,
+            AuthorList
+        },
+        data() {
+            return {
+                actor_list: [
+                    {
+                        name: "Johnny Depp",
+                        pict: "https://img1.closermag.fr/var/closermag/storage/images/bio-people/biographie-johnny-depp-112279/815563-1-fre-FR/Johnny-Depp_square500x500.jpg"
+                    },
+                    {
+                        name: "Leonardo Dicaprio",
+                        pict: "https://img3.closermag.fr/var/closermag/storage/images/1/2/5/7/7/12577149/leonardo-dicaprio-beverly-hills-2004_exact1024x768_p.jpg"
+                    },
+                    {
+                        name: "Johnny Depp",
+                        pict: "https://img1.closermag.fr/var/closermag/storage/images/bio-people/biographie-johnny-depp-112279/815563-1-fre-FR/Johnny-Depp_square500x500.jpg"
+                    },
+                    {
+                        name: "Leonardo Dicaprio",
+                        pict: "https://img3.closermag.fr/var/closermag/storage/images/1/2/5/7/7/12577149/leonardo-dicaprio-beverly-hills-2004_exact1024x768_p.jpg"
+                    },
+                    {
+                        name: "Johnny Depp",
+                        pict: "https://img1.closermag.fr/var/closermag/storage/images/bio-people/biographie-johnny-depp-112279/815563-1-fre-FR/Johnny-Depp_square500x500.jpg"
+                    },
+                    {
+                        name: "Leonardo Dicaprio",
+                        pict: "https://img3.closermag.fr/var/closermag/storage/images/1/2/5/7/7/12577149/leonardo-dicaprio-beverly-hills-2004_exact1024x768_p.jpg"
+                    },
+                    {
+                        name: "Johnny Depp",
+                        pict: "https://img1.closermag.fr/var/closermag/storage/images/bio-people/biographie-johnny-depp-112279/815563-1-fre-FR/Johnny-Depp_square500x500.jpg"
+                    },
+                    {
+                        name: "Leonardo Dicaprio",
+                        pict: "https://img3.closermag.fr/var/closermag/storage/images/1/2/5/7/7/12577149/leonardo-dicaprio-beverly-hills-2004_exact1024x768_p.jpg"
+                    }
+                ],
+                author_list: [
+                    {
+                        name_author: "Spielberg",
+                        role_author: "Réalisateur"
+                    },
+                                        {
+                        name_author: "Spielberg avec un nom vraiment très très long",
+                        role_author: "Réalisateur"
+                    },
+                                        {
+                        name_author: "Spielberg",
+                        role_author: "Réalisateur avec un rôle vraiment très très long"
+                    },
+                                        {
+                        name_author: "Spielberg avec un nom vraiment très très long",
+                        role_author: "Réalisateur avec un rôle vraiment très très long"
+                    },
+                                        {
+                        name_author: "Spielberg",
+                        role_author: "Réalisateur"
+                    },                    {
+                        name_author: "Spielberg",
+                        role_author: "Réalisateur"
+                    },
+                                        {
+                        name_author: "Spielberg avec un nom vraiment très très long",
+                        role_author: "Réalisateur"
+                    },
+                                        {
+                        name_author: "Spielberg",
+                        role_author: "Réalisateur avec un rôle vraiment très très long"
+                    }
+                ]
+            }
+        }
+    }
 </script>
---
+++ front/src/components/AuthorList.vue
@@ -0,0 +1,27 @@
+<template>
+
+    <b-container class="author_class" fluid>
+        <b-row align-v="center" >
+            <Author
+                v-for="author in authors"
+                v-bind:name_author="author.name_author"
+                v-bind:role_author="author.role_author"
+            >
+            </Author>
+        </b-row>
+    </b-container>
+
+
+</template>
+<script>
+    import Author from "./Author.vue";
+    export default {
+        name: "AuthorList",
+        components: {
+            Author,
+        },
+        props: {
+            authors: Array,
+        }
+    }
+</script>
