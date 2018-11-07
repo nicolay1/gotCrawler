be3a523a78c00958b8fb4eaf272a94cbf189ff10
Chapuis Amelie <amelie.chapuis@student.ecp.fr>
2018-11-07 17:09:12 +0100

    completed searchbar component

--- front/src/components/SearchBar.vue
+++ front/src/components/SearchBar.vue
@@ -18,9 +18,17 @@
                 </div>
             </b-col>
         </b-row>
-        <div class="row">
-            <ShowCard :title=title :overview=overview :pict=pict :date_next_ep=date_next_ep
-                      :show_id="show_id"></ShowCard>
+        <div class="row justify-content-md-center">
+            <ShowCard class="showcard_class"
+                v-for="(show, index) in show_list"
+                :key="index"
+                :title="show.title"
+                :overview="show.overview"
+                :pict="show.pict"
+                :date_next_ep="show.date_next_episode"
+                :show_id="show.api_id"
+            >
+            </ShowCard>
         </div>
     </b-container>
 </template>
@@ -37,39 +45,46 @@
             ShowCard
         },
         data() {
-            let list_show = [];
+            // let show_list = [];
             return {
                 show_searched: "Game of Thrones...",
-                list_show: list_show,
+                show_list: Array
 
-                title: "Got",
-                overview: "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.",
-                pict: "https://www.wikichat.fr/wp-content/uploads/sites/2/Fotolia_99751581_S.jpg",
-                date_next_ep: new Date(),
-                show_id: 1295
+                // title: "Got",
+                // overview: "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.",
+                // pict: "https://www.wikichat.fr/wp-content/uploads/sites/2/Fotolia_99751581_S.jpg",
+                // date_next_ep: new Date(),
+                // show_id: 1295
             }
         },
         methods: {
+            // show_results(show_searched) {
+            //     console.log("coucou");
+            //     this.show_list = this.show_results_axios(show_searched);
+            //     console.log("coucou3");
+            // },
             show_results: function (show_searched) {
-                // window.location.href = 'https://fr.wikipedia.org/wiki/' + show_searched;
                 axios
                     .get('http://localhost:5000/show/search', {params:{q: show_searched}})
-                    .then(function (response) {
+                    .then((response)=>{
                         console.log(response);
+                        console.log(response.data);
+                        this.show_list = response.data;
                     })
                     .catch((err) => console.log(err))
-
-
             },
             clean_message: function () {
                 this.show_searched = ""
             }
-        },
+        }
     }
 </script>
 
 <style scoped>
     .search_bar {
         padding: 2px 2px 2px 2px;
+    }
+    .showcard_class {
+        margin: 5px 5px 5px 5px;
     }
 </style>
