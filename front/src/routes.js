3047c80432d8eb1a6baab265a59dee8fab651079
cloune <rachel@nebulae.co>
2018-11-11 22:08:20 +0100

    add user component

--- front/src/routes.js
+++ front/src/routes.js
@@ -2,10 +2,12 @@ import ShowVue from './views/ShowVue.vue'
 import LandingPage from './views/LandingPage.vue'
 import Signup from './components/Signup.vue'
 import Login from './components/Login.vue'
+import User from './components/User.vue'
 
 export default [
     {path: '/', component: LandingPage},
     {path: '/show/:id', component: ShowVue},
     {path: '/login', component: Login},
-    {path: '/signup', component: Signup}
+    {path: '/signup', component: Signup},
+    {path:'/user',component:User}
 ]
